1800e830d38c6778b4eb2dbb95378154686729da
Christy <christymonthomas1999@gmail.com>
2024-04-14 20:13:58 +0530

    Learnings of components in react

---
+++ pdas/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+let el = document.getElementById("root");
+
+let root = ReactDOM.createRoot(el);
+root.render(<App />);
