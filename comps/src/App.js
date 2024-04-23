84796314c219bdd661a1e82408465a7889a4a8fe
Christy <christymonthomas1999@gmail.com>
2024-04-23 07:46:10 +0530

    Initial Commit of comp project

---
+++ comps/src/App.js
@@ -0,0 +1,24 @@
+import Button from "./Button";
+function App() {
+  return (
+    <div>
+      <div>
+        <Button>Hello</Button>
+      </div>
+      <div>
+        <Button>Buy</Button>
+      </div>
+      <div>
+        <Button>Hide Adds</Button>
+      </div>
+      <div>
+        <Button>Hi1</Button>
+      </div>
+      <div>
+        <Button>Hi2</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
